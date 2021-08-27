04b6fd5db6611d34c62e18ec5931f6e57d830724
김혜진 <rlagpwlsq78@gmail.com>
2021-08-27 22:53:25 +0900

    threading 모듈로 스레드 구현
    멀티스레드  md파일에 상세한 내용을 기록

---
+++ temp/multi-thread.py
@@ -0,0 +1,24 @@
+import logging
+from threading import Thread
+import time
+
+
+def work(name, r):
+    """스레드에서 실행할 함수"""
+    logging.info(f"[Sub-thread] {name}: 시작합니다.")
+    for i in r:
+        print(name, i)
+    logging.info(f"[Sub-thread] {name}: 종료합니다.")
+
+
+if __name__ == "__main__":
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
+
+    x = Thread(target=work, args=("A", range(10)))
+    d = Thread(target=work, args=("D", range(10)), daemon=True)
+
+    logging.info("[Main-Thread] 쓰레드 시작 전")
+    x.start()
+    d.start()
+    logging.info("[Main-Thread] 프로그램 종료합니다.")
